9deba26034edd3098ce6b6a63ec38de6d8436d06
AnisDos <anis.bandidos@gmail.com>
2020-05-02 17:01:49 +0100

    rlolm

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -133,8 +133,9 @@ $mounth = array();
 
         $imagePath = request('image')->store('users','public');
         
-       // $image = Image::make(public_path("storage/{$imagePath}"))->fit(120,120);
-        $image = Image::make(public_path("storage/".request('image')))->fit(120,120);
+        //$image = Image::make(public_path("storage/{$imagePath}"))->fit(120,120);
+        $image = Image::make("storage/{$imagePath}")->fit(120,120);
+       
         $image->save();
 
 
